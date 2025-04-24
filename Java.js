86f906b1f226d9b9e7f040cdcf3d2a0a534b4903
vashishthRudra <rudravashishth44@gmail.com>
2025-04-24 11:51:02 +0530

    Modal functionality added

--- Java.js
+++ Java.js
@@ -27,6 +27,16 @@ const closeBtn = document.querySelector('.close');
 joinBtn.addEventListener('click', () => {
     modal.style.display = 'block';
 });
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+
 
 
 // Form Submissions
