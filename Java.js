aedd95aa1a2984732e7a3adb262fc5259affb847
vashishthRudra <rudravashishth44@gmail.com>
2025-04-24 12:34:50 +0530

    Updating JS

--- Java.js
+++ Java.js
@@ -64,7 +64,7 @@ window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
         nav.style.background = 'rgba(0, 0, 0, 0.9)';
     } else {
-        nav.style.background = ;
+        nav.style.background = 'rgba(0, 0, 0, 0.8)';
     }
 });
 
